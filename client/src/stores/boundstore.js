5cf9a0c8f67d068dd5b2cc619aeaaab08fafe3f2
ravioli <victorlecouteur@gmail.com>
2025-01-10 01:45:09 +0100

    delete boardconfig and logic file

--- client/src/stores/boundstore.js
+++ client/src/stores/boundstore.js
@@ -3,8 +3,20 @@ import { createGameSlice } from './gamestore';
 import { createConfigSlice } from './configboardstore';
 import { createBoardSlice } from './boardstore';
 
-export const useBoundStore = create((...a) => ({
-  ...createGameSlice(...a),
-  ...createConfigSlice(...a),
-  ...createBoardSlice(...a),
-}));
+const createSelectors = (_store) => {
+  let store = _store;
+  store.use = {};
+  for (let k of Object.keys(store.getState())) {
+    store.use[k] = () => store((s) => s[k]);
+  }
+
+  return store;
+};
+
+export const useBoundStore = createSelectors(
+  create((...a) => ({
+    ...createGameSlice(...a),
+    ...createConfigSlice(...a),
+    ...createBoardSlice(...a),
+  }))
+);
