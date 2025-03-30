1959053f3d250ffec25484d72a99bdc43564846e
ravioli <victorlecouteur@gmail.com>
2025-03-30 01:38:59 +0100

    refactor history widgets

--- client/src/ui/components/toolbar/history.jsx
+++ client/src/ui/components/toolbar/history.jsx
@@ -9,35 +9,24 @@ import { useModule } from 'src/ui/context/hooks.js';
 
 export const History = () => {
   const module = useModule();
+  const actions = [
+    { icon: IconChevronsLeft, action: 'start' },
+    { icon: IconChevronLeft, action: 'undo' },
+    { icon: IconChevronRight, action: 'redo' },
+    { icon: IconChevronsRight, action: 'end' },
+  ];
+
   return (
     <Group justify="center">
-      <ActionIcon
-        variant="default"
-        size="lg"
-        onClick={() => module.jump('start')}>
-        <IconChevronsLeft size={40} stroke={1.5} />
-      </ActionIcon>
-
-      <ActionIcon
-        variant="default"
-        size="lg"
-        onClick={() => module.jump('undo')}>
-        <IconChevronLeft size={40} stroke={1.5} />
-      </ActionIcon>
-
-      <ActionIcon
-        variant="default"
-        size="lg"
-        onClick={() => module.jump('redo')}>
-        <IconChevronRight size={40} stroke={1.5} />
-      </ActionIcon>
-
-      <ActionIcon
-        variant="default"
-        size="lg"
-        onClick={() => module.jump('end')}>
-        <IconChevronsRight size={40} stroke={1.5} />
-      </ActionIcon>
+      {actions.map(({ icon: Icon, action }) => (
+        <ActionIcon
+          key={action}
+          variant="default"
+          size="lg"
+          onClick={() => module.jump(action)}>
+          <Icon size={40} stroke={1.5} />
+        </ActionIcon>
+      ))}
     </Group>
   );
 };
