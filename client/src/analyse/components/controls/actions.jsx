949109905781ac566461ddd2c79e508e03289082
ravioli <victorlecouteur@gmail.com>
2025-04-10 16:35:51 +0200

    analyse layout rework done

--- client/src/analyse/components/controls/actions.jsx
+++ client/src/analyse/components/controls/actions.jsx
@@ -1,14 +1,16 @@
 import { Group } from '@mantine/core';
-import { Navigate } from 'src/shared/components/toolbar/navigate';
 import { StartButton } from 'src/shared/components/toolbar/start';
 import { FlipButton } from 'src/shared/components/toolbar/flip';
+import { Positions } from './positions';
 import { TestButton } from 'src/shared/components/toolbar/test';
+import { Navigate } from 'src/shared/components/toolbar/navigate';
 
 export const AnalyseActions = () => {
   return (
     <Group justify="center">
       <StartButton />
       <FlipButton />
+      <Positions />
       <TestButton />
       <Navigate path="/editor" />
     </Group>
---
+++ client/src/analyse/components/controls/positions.jsx
@@ -0,0 +1,60 @@
+import { useInitData, useModule } from 'src/shared/hooks/hooks';
+import { Combobox, useCombobox } from '@mantine/core';
+import { ActionIcon, Tooltip } from '@mantine/core';
+import { IconChessRook } from '@tabler/icons-react';
+import { useMemo } from 'react';
+
+export function Positions() {
+  const module = useModule();
+  const positions = useInitData();
+
+  const options = useMemo(
+    () =>
+      positions.map((item) => (
+        <Combobox.Option value={item.fen} key={item.fen}>
+          {[item.eco, item.name].join(' ')}
+        </Combobox.Option>
+      )),
+    [positions]
+  );
+
+  const combobox = useCombobox({
+    onDropdownClose: () => combobox.resetSelectedOption(),
+  });
+
+  return (
+    <>
+      <Combobox
+        store={combobox}
+        width={200}
+        position="top"
+        withinPortal={false}
+        onOptionSubmit={(fen) => {
+          if (fen != module.fen.current()) {
+            module.newGame?.(fen);
+            module.getBoard().position(fen, true);
+            module.fen.setFen(fen);
+          }
+          combobox.closeDropdown();
+        }}>
+        <Combobox.Target>
+          <Tooltip label="select position" position="bottom">
+            <ActionIcon
+              variant="default"
+              size="sm"
+              onClick={() => combobox.toggleDropdown()}
+              styles={{
+                root: { border: 0 },
+              }}>
+              <IconChessRook size={40} stroke={1.2}></IconChessRook>
+            </ActionIcon>
+          </Tooltip>
+        </Combobox.Target>
+
+        <Combobox.Dropdown>
+          <Combobox.Options>{options}</Combobox.Options>
+        </Combobox.Dropdown>
+      </Combobox>
+    </>
+  );
+}
