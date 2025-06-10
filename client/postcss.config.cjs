63309e58d4db26dc037c637697608a70fe0c332b
ravioli <victorlecouteur@gmail.com>
2025-06-10 15:40:08 +0200

    px->rem units

--- client/postcss.config.cjs
+++ client/postcss.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
-    'postcss-preset-mantine': {},
+    'postcss-preset-mantine': { autoRem: true },
     'postcss-simple-vars': {
       variables: {
         'mantine-breakpoint-xs': '36em',
