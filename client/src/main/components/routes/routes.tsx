8ac9dc491a32b213f15b90e5ad0c75f620bf6bfa
Victor <victorlecouteur@gmail.com>
2025-09-25 15:09:12 +0200

    fix shell multiple render

--- client/src/main/components/routes/routes.tsx
+++ client/src/main/components/routes/routes.tsx
@@ -1,26 +1,22 @@
 import { Shell } from 'src/common/components/appshell/appshell';
 import { BrowserRouter, Routes, Route } from 'react-router';
-import { lazy, Suspense } from 'react';
-
-const Analyse = lazy(() => import('src/analyse/components/analyse'));
-const Editor = lazy(() => import('src/editor/components/editor'));
-const Play = lazy(() => import('src/play/components/play'));
-const NotFound = lazy(() => import('./notfound'));
+import Analyse from 'src/analyse/components/analyse';
+import Editor from 'src/editor/components/editor';
+import Play from 'src/play/components/play';
+import NotFound from './notfound';
 
 export const Router = () => {
   return (
     <BrowserRouter>
-      <Suspense>
-        <Routes>
-          <Route path="/" element={<Shell />}>
-            <Route index element={<Analyse />} />
-            <Route path="analysis" element={<Analyse />} />
-            <Route path="editor" element={<Editor />} />
-            <Route path="play" element={<Play />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<Shell />}>
+          <Route index element={<Analyse />} />
+          <Route path="analysis" element={<Analyse />} />
+          <Route path="editor" element={<Editor />} />
+          <Route path="play" element={<Play />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 };
