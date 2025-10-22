a7873833034ab71abe4e5aa6ee0479b7f6dab283
Victor <victorlecouteur@gmail.com>
2025-10-22 20:10:06 +0200

    changing cookie samesite

--- server/src/profile/views.py
+++ server/src/profile/views.py
@@ -57,7 +57,7 @@ class ProfileViewSet(viewsets.GenericViewSet):
             max_age=datetime.timedelta(days=365),
             secure=settings.SSL,
             httponly=False,
-            samesite="Lax",
+            samesite="Strict",
         )
         return response
 
