61b7db7140697c13a315faa1a96811163b5d07e5
Victor <victorlecouteur@gmail.com>
2026-02-27 19:35:55 +0100

    jinja_vite utilities folders

--- server/app/web/routes.py
+++ server/app/web/routes.py
@@ -6,10 +6,16 @@ from jinja2 import Environment, PackageLoader, select_autoescape
 env = Environment(
     autoescape=select_autoescape(["html", "htm", "xml"]),
     loader=PackageLoader("app.web", "templates"),
+    trim_blocks=True,
+    lstrip_blocks=True,
 )
+
 templates = Jinja2Templates(env=env)
 
 router = APIRouter(prefix="", tags=["web"])
+
+# todo: build a small django-vite layer using globals (a config and some globals)
+# add context_processor
 
 
 @router.get("/", response_class=HTMLResponse, name="root")
